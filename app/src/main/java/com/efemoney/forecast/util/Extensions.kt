41afd77a0d8e6b65f46ef948fc4f96d5a1c76598
Efeturi Money <Efe@efeturimbp.local>
2017-06-16 02:59:46 +0100

    refactor: Add View.invisible extension

--- app/src/main/java/com/efemoney/forecast/util/Extensions.kt
+++ app/src/main/java/com/efemoney/forecast/util/Extensions.kt
@@ -2,9 +2,14 @@ package com.efemoney.forecast.util
 
 import android.view.View
 
+fun View.gone() {
+    visibility = View.GONE
+}
+
 fun View.visible() {
     visibility = View.VISIBLE
 }
-fun View.gone() {
-    visibility = View.GONE
+
+fun View.invisible() {
+    visibility = View.INVISIBLE
 }
